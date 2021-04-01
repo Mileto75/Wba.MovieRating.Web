bf6e8c9566e7a36849f9d4fd30a41811ad387a9c
Mileto Di Marco <mileto.di.marco@howest.be>
2021-04-01 13:56:00 +0200

    added seeding

--- Wba.MovieRating.Web/Data/DataSeeder.cs
+++ Wba.MovieRating.Web/Data/DataSeeder.cs
@@ -13,13 +13,75 @@ namespace Wba.MovieRating.Web.Data
         {
             //seeding data klaarzetten
             //denk aan de volgorde!!
+            //companies
             var companies = new Company[]
             {
                 new Company{Id=1,Name="Warner Bros"},
                 new Company{Id=2,Name="Universal"},
                 new Company{Id=3,Name="Pixar"}
             };
-            //
+            //users
+            var users = new User[]
+            {
+                new User{Id=1,FirstName="Slash", LastName="Slash",Username="slash@GandR.com" },
+                new User{Id=2,FirstName="John", LastName="Frusciante",Username="johnF@RedHotChillis.com" }
+            };
+            //Actors
+            var actors = new Actor[]
+            {
+                new Actor{Id=1,FirstName="Keanu",LastName="Reeves"},
+                new Actor{Id=2,FirstName="Kim",LastName="Basinger"},
+                new Actor{Id=3,FirstName="Robert",LastName="De Niro"},
+                new Actor{Id=4,FirstName="Walter",LastName="Capiau"},
+            };
+            //Directors
+            var directors = new Director[]
+            {
+                new Director{Id=1,FirstName="Steven",LastName="Spielberg"},
+                new Director{Id=2,FirstName="Sly",LastName="Stallone"},
+                new Director{Id=3,FirstName="Kathryn",LastName="Bigelow"}
+            };
+            //movies
+            var movies = new Movie[]
+            {
+                new Movie{Id=1,Title="Point Break",CompanyId=1 },
+                new Movie{Id=2,Title="Rocky",CompanyId=2 },
+                new Movie{Id=3,Title="Rambo",CompanyId=1 }
+            };
+            //MovieActors
+            var movieActors = new MovieActors[]
+            {
+                new MovieActors{ActorId=1,MovieId=1},
+                new MovieActors{ActorId=2,MovieId=3},
+                new MovieActors{ActorId=2,MovieId=2},
+                new MovieActors{ActorId=3,MovieId=3}
+            };
+            //movieDirectors
+            var movieDirectors = new MovieDirectors[]
+            {
+                new MovieDirectors{MovieId=1,DirectorId=1},
+                new MovieDirectors{MovieId=2,DirectorId=2},
+                new MovieDirectors{MovieId=3,DirectorId=3}
+            };
+            //Ratings
+            var ratings = new Rating[]
+            {
+                new Rating{Id=1, UserId=1,MovieId=1,Score=10,Review="Very good! Loved it!"},
+                new Rating{Id=2,UserId=2,MovieId=3,Score=4,Review="Sucks monkeyballs!"},
+                new Rating{Id=3,UserId=2,MovieId=3,Score=10,Review="Effing awesome!!"}
+            };
+
+            //call the hasdata methods for each entity
+            //mind the volgorde!!
+            modelBuilder.Entity<Company>().HasData(companies);
+            modelBuilder.Entity<Actor>().HasData(actors);
+            modelBuilder.Entity<Director>().HasData(directors);
+            modelBuilder.Entity<User>().HasData(users);
+            modelBuilder.Entity<Movie>().HasData(movies);
+            modelBuilder.Entity<MovieDirectors>().HasData(movieDirectors);
+            modelBuilder.Entity<MovieActors>().HasData(movieActors);
+            modelBuilder.Entity<Rating>().HasData(ratings);
+            
         }
     }
 }
---
+++ Wba.MovieRating.Web/Migrations/20210401115404_addedSeeding.cs
@@ -0,0 +1,224 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Wba.MovieRating.Web.Migrations
+{
+    public partial class addedSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1L, "Keanu", "Reeves" },
+                    { 2L, "Kim", "Basinger" },
+                    { 3L, "Robert", "De Niro" },
+                    { 4L, "Walter", "Capiau" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1L, "Warner Bros" },
+                    { 2L, "Universal" },
+                    { 3L, "Pixar" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Directors",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1L, "Steven", "Spielberg" },
+                    { 2L, "Sly", "Stallone" },
+                    { 3L, "Kathryn", "Bigelow" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName", "Username" },
+                values: new object[,]
+                {
+                    { 1L, "Slash", "Slash", "slash@GandR.com" },
+                    { 2L, "John", "Frusciante", "johnF@RedHotChillis.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "CompanyId", "ReleaseDate", "Title" },
+                values: new object[] { 1L, 1L, null, "Point Break" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "CompanyId", "ReleaseDate", "Title" },
+                values: new object[] { 3L, 1L, null, "Rambo" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "CompanyId", "ReleaseDate", "Title" },
+                values: new object[] { 2L, 2L, null, "Rocky" });
+
+            migrationBuilder.InsertData(
+                table: "MovieActors",
+                columns: new[] { "ActorId", "MovieId" },
+                values: new object[,]
+                {
+                    { 1L, 1L },
+                    { 2L, 3L },
+                    { 3L, 3L },
+                    { 2L, 2L }
+                });
+
+            migrationBuilder.InsertData(
+                table: "MovieDirectors",
+                columns: new[] { "DirectorId", "MovieId" },
+                values: new object[,]
+                {
+                    { 1L, 1L },
+                    { 3L, 3L },
+                    { 2L, 2L }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Ratings",
+                columns: new[] { "Id", "MovieId", "Review", "Score", "UserId" },
+                values: new object[,]
+                {
+                    { 1L, 1L, "Very good! Loved it!", 10, 1L },
+                    { 2L, 3L, "Sucks monkeyballs!", 4, 2L },
+                    { 3L, 3L, "Effing awesome!!", 10, 2L }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Actors",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "MovieActors",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 1L, 1L });
+
+            migrationBuilder.DeleteData(
+                table: "MovieActors",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 2L, 2L });
+
+            migrationBuilder.DeleteData(
+                table: "MovieActors",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 2L, 3L });
+
+            migrationBuilder.DeleteData(
+                table: "MovieActors",
+                keyColumns: new[] { "ActorId", "MovieId" },
+                keyValues: new object[] { 3L, 3L });
+
+            migrationBuilder.DeleteData(
+                table: "MovieDirectors",
+                keyColumns: new[] { "DirectorId", "MovieId" },
+                keyValues: new object[] { 1L, 1L });
+
+            migrationBuilder.DeleteData(
+                table: "MovieDirectors",
+                keyColumns: new[] { "DirectorId", "MovieId" },
+                keyValues: new object[] { 2L, 2L });
+
+            migrationBuilder.DeleteData(
+                table: "MovieDirectors",
+                keyColumns: new[] { "DirectorId", "MovieId" },
+                keyValues: new object[] { 3L, 3L });
+
+            migrationBuilder.DeleteData(
+                table: "Ratings",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Ratings",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Ratings",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Actors",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Actors",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Actors",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Directors",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Directors",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Directors",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2L);
+        }
+    }
+}
